ddafeded72c325bacf3b3307d535c5e856d14ac1
Murilo <murilosomacal5@gmail.com>
2026-05-21 22:11:00 -0300

    html feito e js no processo,  nada css

---
+++ forms.js
@@ -0,0 +1,27 @@
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const senha = document.getElementById("password");
+const botao = document.getElementById("botao");
+
+const emailFixo = "user@gmail.com";
+const senhaFixa = "12345";
+
+function validarEmail(email){
+    if (email === '' || email === null || email === undefined){
+        console.log('Erro: Campo obrigatório');
+        return false;
+    }
+    console.log('Campo válido');
+    return true;
+}
+
+
+
+
+
+
+
+function validarEmail(email) {
+     return email.includes('@') && email.includes('.');
+}
+    
