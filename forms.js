392a94f5dc6b5c334e35ee6c2f2d693ec358114a
Murilo <murilosomacal5@gmail.com>
2026-05-21 22:13:07 -0300

    correção

--- forms.js
+++ forms.js
@@ -3,7 +3,7 @@ const email = document.getElementById("email");
 const senha = document.getElementById("password");
 const botao = document.getElementById("botao");
 
-const emailFixo = "user@gmail.com";
+const emailFixo = "admin@gmail.com";
 const senhaFixa = "12345";
 
 function validarEmail(email){
